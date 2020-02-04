49b41f52eeab4be3465746b683ecb929f427823d
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-04 15:34:41 +0900

    【new】exam02-result.htmlで足し算結果を表示する。

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,21 +1,31 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/exam02")
 public class Exam02Controller {
 
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("")
 	public String index(){
 		return "exam02";
 	}
 	
 	@RequestMapping("/receive-form")
-	public String receiveForm(String name, Model model){
-		model.addAttribute("name",name);
+	public String receiveForm(Integer num1,Integer num2){
+		int answer = num1 + num2;
+		session.setAttribute("num1",num1);
+		session.setAttribute("num2",num2);
+		session.setAttribute("answer",answer);
 		return "exam02-result";
 	}
 }
